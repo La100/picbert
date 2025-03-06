48ca3cdcf57d6743d7b5bb3a8ef0dc64519d78a0
La100 <stolamarcin@gmail.com>
2025-03-06 16:04:57 +0100

    elo

--- src/components/billing/UpgradeBtn.tsx
+++ src/components/billing/UpgradeBtn.tsx
@@ -2,11 +2,20 @@
 
 import Link from "next/link"
 import { RainbowButton } from "../ui/rainbow-button"
+import { useSidebar } from "../ui/sidebar"
 
 export default function UpgradeBtn() {
+  const { isMobile, setOpenMobile } = useSidebar()
+
+  const handleClick = () => {
+    // Close the mobile sidebar when clicked
+    if (isMobile) {
+      setOpenMobile(false)
+    }
+  }
 
   return (
-    <Link href="/billing" className="inline-block w-full">
+    <Link href="/billing" className="inline-block w-full" onClick={handleClick}>
       <RainbowButton 
         className="w-full text-sm font-semibold rounded-md mb-2"
       >
